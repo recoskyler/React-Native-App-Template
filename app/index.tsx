dc67172dfe831f755e1bdf1205cb8a272c38ae56
Adil Atalay Hamamcioglu <recoskyler224@gmail.com>
2020-12-06 01:25:31 +0200

    Added necessary formatting and linting Completed project setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
 
 export default function App() {
   return (
@@ -10,12 +19,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
